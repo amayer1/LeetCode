f6db71e8c03893fa7cdb9fbba32d4af8324121b6
Andrew Mayer <andrewmayer0@gmail.com>
2018-10-28 18:56:36 -0400

    Adding node to binary tree

---
+++ 701.cpp
@@ -0,0 +1,16 @@
+//Simple adding node to binary tree
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val)
+    {
+        if(root == NULL)
+        {
+            return new TreeNode(val);         
+        }
+        if(val > root->val)   
+            root->right = insertIntoBST(root->right, val);
+        else
+            root->left = insertIntoBST(root->left, val);
+        return root;
+    }
+};
