5f12116326ed0341ee3c6b97b32c366d337a904f
Andrew Mayer <andrewmayer0@gmail.com>
2018-12-06 17:49:04 -0500

    Min Cost Climbing Stairs

---
+++ 746.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) 
+    {
+        for(int i = 2; i < cost.size(); i++)
+            cost[i] = std::min(cost[i-1], cost[i-2]) + cost[i];
+        return std::min(cost.back(), cost[cost.size()-2]);
+    }
+};
